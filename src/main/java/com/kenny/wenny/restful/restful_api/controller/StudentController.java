1073e3baa01ebed6dc84882f5a3bc4afaff1ec38
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-05-29 19:41:29 +0100

    Response entity first example

--- src/main/java/com/kenny/wenny/restful/restful_api/controller/StudentController.java
+++ src/main/java/com/kenny/wenny/restful/restful_api/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.kenny.wenny.restful.restful_api.controller;
 
 import com.kenny.wenny.restful.restful_api.model.Student;
 import lombok.val;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +14,19 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentController {
     @GetMapping
-    @ResponseBody
-    public List<Student> getStudent() {
+    public ResponseEntity<List<Student>> getStudent() {
         val firstStudent = Student.builder()
                 .age(25)
                 .name("John")
                 .course("Java")
                 .build();
         val secondStudent = Student.builder()
-                .age(30)
-                .name("Barbara")
+                .age(90)
+                .name("Steve")
                 .course("Python")
                 .build();
-        return List.of(firstStudent, secondStudent);
+        return ResponseEntity.ok(
+                List.of(firstStudent, secondStudent)
+        );
     }
 }
