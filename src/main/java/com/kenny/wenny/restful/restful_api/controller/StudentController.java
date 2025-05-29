79dc5a3fb713b73dcece2308d84055e24e1d1f41
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-05-29 19:35:52 +0100

    Class for a response object

---
+++ src/main/java/com/kenny/wenny/restful/restful_api/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.kenny.wenny.restful.restful_api.controller;
+
+import com.kenny.wenny.restful.restful_api.model.Student;
+import lombok.val;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping
+    @ResponseBody
+    public List<Student> getStudent() {
+        val firstStudent = new Student();
+        firstStudent.setAge(25);
+        firstStudent.setName("John");
+        firstStudent.setCourse("Java");
+        val secondStudent = new Student();
+        secondStudent.setAge(44);
+        secondStudent.setName("Cynthia");
+        secondStudent.setCourse("Python");
+        return List.of(firstStudent, secondStudent);
+    }
+}
---
+++ src/main/java/com/kenny/wenny/restful/restful_api/model/Student.java
@@ -0,0 +1,10 @@
+package com.kenny.wenny.restful.restful_api.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private int age;
+    private String course;
+}
