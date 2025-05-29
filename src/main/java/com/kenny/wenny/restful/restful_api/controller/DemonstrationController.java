a8012ec6244519afb2528c67c26b940b74f4e0d6
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-05-29 19:30:04 +0100

    GetMapping annotation

--- src/main/java/com/kenny/wenny/restful/restful_api/controller/DemonstrationController.java
+++ src/main/java/com/kenny/wenny/restful/restful_api/controller/DemonstrationController.java
@@ -1,13 +1,14 @@
 package com.kenny.wenny.restful.restful_api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemonstrationController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/violeta")
     @ResponseBody
     public String get() {
         return "demonstration";
